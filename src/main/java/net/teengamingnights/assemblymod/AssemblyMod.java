05fea7039200be95c611d749db0419a83d01e596
aecsocket <aecsocket@tutanota.com>
2018-12-23 20:41:48 +0000

    Finally finish creating factories
    Signed-off-by: aecsocket <aecsocket@tutanota.com>

--- src/main/java/net/teengamingnights/assemblymod/AssemblyMod.java
+++ src/main/java/net/teengamingnights/assemblymod/AssemblyMod.java
@@ -1,14 +1,24 @@
 package net.teengamingnights.assemblymod;
 
+import net.teengamingnights.assemblymod.factory.FactoryManager;
+import net.teengamingnights.assemblymod.factory.TestFactory;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AssemblyMod extends JavaPlugin {
     private static AssemblyMod instance;
+    private static FactoryManager factoryManager;
+
     public static AssemblyMod getInstance() { return instance; }
+    public static FactoryManager getFactoryManager() { return factoryManager; }
 
     @Override
     public void onEnable() {
         instance = this;
+        factoryManager = new FactoryManager();
+
         saveDefaultConfig();
+        Bukkit.getPluginManager().registerEvents(new EventHandle(), this);
+        factoryManager.registerFactory(new TestFactory());
     }
 }
