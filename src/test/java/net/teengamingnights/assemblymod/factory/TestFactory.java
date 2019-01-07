8b80415533f29678097583c1fa9e0f9c92d0898a
Daffehh <simplemodified@gmail.com>
2019-01-06 19:03:53 -0800

    ahhhh test
    Signed-off-by: Daffehh <simplemodified@gmail.com>

--- src/test/java/net/teengamingnights/assemblymod/factory/TestFactory.java
+++ src/test/java/net/teengamingnights/assemblymod/factory/TestFactory.java
@@ -1,13 +1,49 @@
 package net.teengamingnights.assemblymod.factory;
 
-import net.teengamingnights.assemblymod.factory.Factory;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class TestFactory implements Factory {
+    @Override
+    public UUID getId() {
+        return null;
+    }
+
+    @Override
+    public Location getCenter() {
+        return null;
+    }
+
+    @Override
+    public Location getFurnace() {
+        return null;
+    }
+
+    @Override
+    public Location getChest() {
+        return null;
+    }
+
+    @Override
+    public FactoryType getType() {
+        return null;
+    }
+
+    @Override
+    public double getHealth() {
+        return 0;
+    }
+
+    @Override
+    public double getHealthLossMultiplier() {
+        return 0;
+    }
+
     @Override
     public List<ItemStack> getRequiredMaterials() {
         return new ArrayList<ItemStack>() {{
